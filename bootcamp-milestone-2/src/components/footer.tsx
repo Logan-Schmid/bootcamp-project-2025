e8eaf7d5c69cf6a8b0c49a7015dfcaa0d8a7c5ad
Logan-Schmid <lschmid013@gmail.com>
2025-10-19 00:03:41 -0700

    feat: add Footer component and use in layout

---
+++ bootcamp-milestone-2/src/components/footer.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./footer.module.css";
+
+export default function Footer() {
+  return (
+    <footer className={styles.footer}>
+      © 2025 Logan-Schmid.github.io | All Rights Reserved
+    </footer>
+  );
+}
