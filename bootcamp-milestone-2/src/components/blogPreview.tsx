42356857b7a99f19f1703f210cc69a9fb3aefdd6
Logan-Schmid <lschmid013@gmail.com>
2025-10-19 11:50:43 -0700

    style: receive all props in 'props' parameter instead of each Blog attribute explicitly

--- bootcamp-milestone-2/src/components/blogPreview.tsx
+++ bootcamp-milestone-2/src/components/blogPreview.tsx
@@ -3,27 +3,23 @@ import styles from "./blogPreview.module.css";
 import Image from "next/image";
 import { Blog } from "@/app/blogData";
 
-export default function BlogPreview({
-  title,
-  date,
-  description,
-  image,
-  imageAlt,
-}: Blog) {
+export default function BlogPreview(props: Blog) {
   return (
     <div id={styles["blog-container"]}>
       <div className={styles["blog-entry"]}>
-        <h2 className={styles["blog-entry-title"]}>{title}</h2>
+        <h2 className={styles["blog-entry-title"]}>{props.title}</h2>
         <div>
           <Image
             className={styles["blog-entry-image"]}
-            src={image}
-            alt={imageAlt}
+            src={props.image}
+            alt={props.imageAlt}
             width={300}
             height={300}
           ></Image>
-          <p className={styles["blog-entry-description"]}>{description}</p>
-          <p className={styles["blog-entry-date"]}>{date}</p>
+          <p className={styles["blog-entry-description"]}>
+            {props.description}
+          </p>
+          <p className={styles["blog-entry-date"]}>{props.date}</p>
         </div>
       </div>
     </div>
