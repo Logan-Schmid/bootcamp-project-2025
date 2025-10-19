2a8c1c9e36626b6a04b0b60e08e3ab14f4d1a949
Logan-Schmid <lschmid013@gmail.com>
2025-10-18 23:36:23 -0700

    feat: initial commit

---
+++ bootcamp-milestone-2/src/blog/page.tsx
@@ -0,0 +1,14 @@
+export default function Blog() {
+  return (
+    <>
+      <main>
+        <h1 className="page-title">Blog</h1>
+        <div id="blog-container"></div>
+      </main>
+      <footer className="footer">
+        © 2025 Logan-Schmid.github.io | All Rights Reserved
+      </footer>
+      <script src="src/blog.js"></script>
+    </>
+  );
+}
