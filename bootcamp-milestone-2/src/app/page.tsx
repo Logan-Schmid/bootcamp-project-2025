5266a6051399100aa48c3dcc24526d7368a6f4a5
Logan-Schmid <lschmid013@gmail.com>
2025-10-19 00:10:04 -0700

    feat: add html for homepage

--- bootcamp-milestone-2/src/app/page.tsx
+++ bootcamp-milestone-2/src/app/page.tsx
@@ -1,3 +1,27 @@
 export default function Home() {
-  return <div>// content will go within here.</div>;
+  return (
+    <main>
+      <h1 className="page-title">Hi, I'm Logan Schmid</h1>
+      <div className="about">
+        <div className="about-text">
+          <p>
+            I am a <strong>BMED grad student</strong> at <em>Cal Poly</em>.
+          </p>
+          <p>
+            I am currently working on my Master's thesis, involving the use of{" "}
+            <em>pressure sensors</em> and <em>deep learning</em> models in an{" "}
+            <em>embedded system</em> to analyze the fit of a{" "}
+            <em>lower-limb prosthetic</em> on an amputee's residual limb.
+          </p>
+        </div>
+        <div className="about-image">
+          <img
+            src="/LLEAP Headshot 2023.png"
+            alt="Picture of me!"
+            width="250"
+          />
+        </div>
+      </div>
+    </main>
+  );
 }
